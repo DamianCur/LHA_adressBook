dd6cdf02ab129d17f2040367286a46d1ed00e615
Damian Cur <damian.cur91@gmail.com>
2021-09-07 19:45:57 +0200

    fixed string validation function

--- task/utility.js
+++ task/utility.js
@@ -1,4 +1,4 @@
-const stringValidation = () => {
+const stringValidation = (key, value) => {
     if (typeof key !== "string" || key.length === 0) throw Error("You have to type in what you want to change.")
     if (typeof value !== "string" || value.length === 0) throw Error("You have to type in value.")
 }
