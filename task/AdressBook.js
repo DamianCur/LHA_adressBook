6c1b0d57885c27204ce418fb7b38db748cf88a91
Damian Cur <damian.cur91@hmail.com>
2021-10-07 21:08:08 +0200

    added elemetary logic

---
+++ task/AdressBook.js
@@ -0,0 +1,21 @@
+import Contact from './Contact.js'
+
+class AddressBook {
+    constructor() {
+        this.contactList = []
+        this.groupList = []
+    }
+
+    searchContact(value){
+
+    }
+
+    //jak uzyskać dostęp do metod z innych klas?
+
+    // Ma umożliwiać: szukanie kontaktu po frazie, dodawanie/usuwanie/modyfikacje nowych kontaktów, 
+    // dodawanie/usuwanie/modyfikacje nowych grup
+
+    
+
+
+}
