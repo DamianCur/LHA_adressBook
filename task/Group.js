84e0c2160c81a0fc3abf9b5b233fcb0fff1abde1
Damian Cur <damian.cur91@hmail.com>
2021-10-07 19:25:47 +0200

    added changeGroupName method

--- task/Group.js
+++ task/Group.js
@@ -2,22 +2,26 @@ import {
     v4 as uuidv4
 } from 'uuid';
 
-import Contact from "./Contact.js"
 
 
 
 class Group {
     constructor(groupName) {
         this.constactList = []
-        this.group = groupName
+        this.groupName = groupName
         this.uuid = uuidv4()
     }
 
     addContact(value) {
-       
+
         // if(!value instanceof Contact) throw Error ("The object isn't an instance of Contact class")
         //poprawic niedziałającą walidację
         this.constactList.push(value)
+    }
+
+    changeGroupName(newGroupName) {
+        if (typeof newGroupName !== "string" || newGroupName.length === 0) throw Error(`${newGroupName} is not a valid value.`)
+        this.groupName = newGroupName
     }
 
 }
