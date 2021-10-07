1bea46ff2ede3504aa181ef70b82c4a18abd1d29
Damian Cur <damian.cur91@hmail.com>
2021-10-07 20:49:35 +0200

    testing

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 import Contact from "./task/Contact.js";
 import Group from "./task/Group.js";
 const damian = new Contact("Damian", "Cur", "email", "")
-const contactList = new Group("praca")
-contactList.addContact(damian)
-console.log(contactList)
+const natalia = new Contact("Natalia", "Żymełka", "asdasd@asdas.pl")
+const friends = new Group("Znajomi")
+const familly = new Group ("Rodzina")
+friends.addContact(damian)
+familly.addContact(natalia)
+
+friends.isInGroup(natalia)
+
+
+
+
+
